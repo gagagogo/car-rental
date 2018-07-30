6e3d64109b423baf3be02cca93eb99d5eb1763da
gagagogo <gagagogo@ukr.net>
2018-07-30 18:37:36 +0300

    make messagint setup

--- test-task-vehicle-tracker/src/main/java/ru/domru/testtaskvehicletracker/VehicleTrackPoint/VehicleTrackPoint.java
+++ test-task-vehicle-tracker/src/main/java/ru/domru/testtaskvehicletracker/VehicleTrackPoint/VehicleTrackPoint.java
@@ -1,9 +1,11 @@
 package ru.domru.testtaskvehicletracker.VehicleTrackPoint;
 
 import java.math.BigDecimal;
-import java.security.Timestamp;
+import java.sql.Timestamp;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 /**
  * JPA Entity for storing GPS points of vehicle track
@@ -13,14 +15,77 @@ import javax.persistence.Entity;
 @Entity
 public class VehicleTrackPoint {
 	
+	@Id
+	@Column
 	private Long idVehicleTrackPoint;
 	
+	@Column
 	private Timestamp creationTime;
 	
+	@Column	
 	private Timestamp creationDate;
 	
+	@Column
 	private BigDecimal latitudes;
 	
+	@Column
 	private BigDecimal longitudes;
+
+	public VehicleTrackPoint(Long idVehicleTrackPoint, Timestamp creationTime, Timestamp creationDate,
+			BigDecimal latitudes, BigDecimal longitudes) {
+		this.idVehicleTrackPoint = idVehicleTrackPoint;
+		this.creationTime = creationTime;
+		this.creationDate = creationDate;
+		this.latitudes = latitudes;
+		this.longitudes = longitudes;
+	}
+
+	public VehicleTrackPoint() {
+	}
+
+	public Long getIdVehicleTrackPoint() {
+		return idVehicleTrackPoint;
+	}
+
+	public void setIdVehicleTrackPoint(Long idVehicleTrackPoint) {
+		this.idVehicleTrackPoint = idVehicleTrackPoint;
+	}
+
+	public Timestamp getCreationTime() {
+		return creationTime;
+	}
+
+	public void setCreationTime(Timestamp creationTime) {
+		this.creationTime = creationTime;
+	}
+
+	public Timestamp getCreationDate() {
+		return creationDate;
+	}
+
+	public void setCreationDate(Timestamp creationDate) {
+		this.creationDate = creationDate;
+	}
+
+	public BigDecimal getLatitudes() {
+		return latitudes;
+	}
+
+	public void setLatitudes(BigDecimal latitudes) {
+		this.latitudes = latitudes;
+	}
+
+	public BigDecimal getLongitudes() {
+		return longitudes;
+	}
+
+	public void setLongitudes(BigDecimal longitudes) {
+		this.longitudes = longitudes;
+	}
+	
+	
+
+	
+	
 	
 }
--- test-task-vehicle-tracker/src/main/java/ru/domru/testtaskvehicletracker/web/VehicleTrackerController.java
+++ test-task-vehicle-tracker/src/main/java/ru/domru/testtaskvehicletracker/web/VehicleTrackerController.java
@@ -1,8 +1,31 @@
 package ru.domru.testtaskvehicletracker.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.integration.support.MessageBuilder;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import ru.domru.testtaskvehicletracker.VehicleTrackPoint.VehicleTrackPoint;
+import ru.domru.testtaskvehicletracker.messaging.VehicleTrackingPoint;
 
 @RestController
+@RequestMapping("api/vehicle-tracker")
+@EnableBinding(VehicleTrackingPoint.Sink.class)
 public class VehicleTrackerController {
 	
+	@Autowired
+	VehicleTrackingPoint.Sink vehicleTrackingMessageInput;
+	
+	@RequestMapping("point/write")
+	Mono<ResponseEntity<String>> writePoint(@RequestBody VehicleTrackPoint vehicleTrackPoint){
+		//boolean res = vehicleTrackingMessageInput.output().send(MessageBuilder.withPayload(vehicleTrackPoint).build());
+		//if(res) 
+			return Mono.just(ResponseEntity.ok("Successfuly saved"));
+		//return Mono.just(ResponseEntity.status(HttpStatus.BAD_REQUEST).build());
+	}
+
 }
