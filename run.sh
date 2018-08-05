910682ccedfc72ded3864a1e9c6c16ba9e0ee9c3
gagagogo <gagagogo@ukr.net>
2018-08-05 18:00:13 +0300

    change maven project structure. Made modules

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mvn -f ./app/pom.xml clean package -Dmaven.test.skip=true
+mvn -f ./car-rental-web-ui/pom.xml clean package -Dmaven.test.skip=true
 mvn -f ./test-task-vehicle-tracker/pom.xml clean package -Dmaven.test.skip=true
-docker image build -t car-rental-web-ui:base ./app
+docker image build -t car-rental-web-ui:base ./car-rental-web-ui
 docker image build -t car-rental-tracking:base ./test-task-vehicle-tracker
 docker image build -t car-rental-nginx:base ./nginx
 docker swarm init --advertise-addr=127.0.0.1
